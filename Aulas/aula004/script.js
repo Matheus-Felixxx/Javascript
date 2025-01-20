27178b8ddcead84562947bec686348027ca2d8c6
Matheus-Felixxx <matheusfelixf862@gmail.com>
2025-01-20 19:27:00 -0300

    scripts

---
+++ Aulas/aula004/script.js
@@ -0,0 +1,3 @@
+var nome = window.prompt('Qual e seu nome?')
+        document.write(`Olá ${nome}. Seu nome tem ${nome.length} letras`)
+        document.write(`Seu nome em maíusculas é ${nome.toLowerCase()}`)

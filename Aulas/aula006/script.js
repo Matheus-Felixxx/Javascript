d1431cdf7e2809dce216f4c3ea8018878325b88b
Matheus-Felixxx <matheusfelixf862@gmail.com>
2025-02-05 19:26:42 -0300

    aula 6

---
+++ Aulas/aula006/script.js
@@ -0,0 +1,13 @@
+var body = window.document.body
+var p1 = window.document.getElementsByTagName('p')[0]
+p1.style.color = `blue`
+body.style.color = `black`
+/*
+var div = window.document.getElementById('div')
+div.style.background = `green`
+div.style.color = `white`
+div.innerText = 'Estou aguadando...'
+*/
+var d = window.document.querySelector('div#div')
+d.style.color = `blue`
+d.style.background = `white`

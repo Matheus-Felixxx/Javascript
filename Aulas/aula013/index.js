41d696b9f8f927ec8c50f07145d27cb9526147ab
Matheus-Felixxx <matheusfelixf862@gmail.com>
2025-02-17 19:54:30 -0300

    aula 13

---
+++ Aulas/aula013/index.js
@@ -0,0 +1,11 @@
+var idade = 18
+
+if (idade < 18){
+    console.log('Menor de idade')
+} else{
+    if (idade == 18){
+        console.log('igual a dezoito')
+    } else{
+        console.log('Maior de idade')
+    }
+}

2bce2746b3a54a5f381d2afb734a34d8ff77df81
Matheus-Felixxx <matheusfelixf862@gmail.com>
2025-02-21 20:21:37 -0300

    desafios criados

--- Aulas/aula013/index.js
+++ Aulas/aula013/index.js
@@ -1,11 +1,11 @@
-var idade = 18
-
-if (idade < 18){
-    console.log('Menor de idade')
-} else{
-    if (idade == 18){
-        console.log('igual a dezoito')
+var idade = 3
+console.log(`Você tem ${idade} anos`)
+if (idade < 16){
+    console.log('Não Vota')
+} else{   if (idade < 18 || idade > 65){
+        console.log('Voto opcional') 
     } else{
-        console.log('Maior de idade')
+        console.log('voto obrigatorio') 
     }
 }
+ 

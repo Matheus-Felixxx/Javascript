25f61bb069786a9fd46d91d57cd465457893c962
Matheus-Felixxx <matheusfelixf862@gmail.com>
2025-03-03 15:42:10 -0300

    aula 16

---
+++ Aulas/aula016/index.js
@@ -0,0 +1,20 @@
+var c = 1
+do{
+    console.log(`Hello World ${c}`)
+    c++
+} while(c <= 10)
+
+/*while (c <= 8 ){
+    console.log(`passo ${c}`)
+    c++
+}*/
+
+/*console.log('Hello World!')
+console.log('Hello World!')
+console.log('Hello World!')
+console.log('Hello World!')
+console.log('Hello World!')
+console.log('Hello World!')
+console.log('Hello World!')
+console.log('Hello World!')
+*/

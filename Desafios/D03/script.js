fef4c97c7bee9137c724539458849345c814339a
Matheus-Felixxx <matheusfelixf862@gmail.com>
2025-03-12 19:31:35 -0300

    final desafio 3

--- Desafios/D03/script.js
+++ Desafios/D03/script.js
@@ -2,10 +2,33 @@ function contar(){
     var ini = document.getElementById('inum') 
     var fim = document.getElementById('ifim')
     var pas = document.getElementById('ipass')
+    var res = document.getElementById('rest')
 
     if(ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0)   {
-        window.alert('[ERROR] Faltam dados >:(')
+        res.innerHTML = 'Impossivel contar!'
+        //window.alert('[ERROR] Faltam dados >:(')
+        
     } else{
-        window.alert('Tudo certo')
+       res.innerHTML = 'Contando: <br> '
+       var i = Number(ini.value)
+       var f = Number(fim.value)
+       var p = Number(pas.value)
+       if(p <= 0){
+        window.alert('Passo Inválido! considerando PASSO 1')
+        p = 1 
+       }
+       // Contagem crescente
+        if(i < f){
+       for(var c = i; c <= f; c += p) {
+        res.innerHTML += `${c} \u{1F449} `
+       }
+       res.innerHTML += `\u{1F6A9}`
+       //contagem regressiva
+    } else{
+        for(let c = i; c >= f; c -= p){
+            res.innerHTML += `${c} \u{1F449}`
+        }
+        res.innerHTML += `\u{1F6A9}`
+    }
     }
 }
